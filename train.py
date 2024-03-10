b4d2b66a7a25788e0d6781c8f7df08247aaf9afc
Ubuntu <ubuntu@ip-172-31-41-229.ec2.internal>
2024-03-10 04:17:08 +0000

    trained model

--- train.py
+++ train.py
@@ -6,17 +6,15 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 # Use the model
 results = model.train(
     data="config.yaml",
-    epochs=100,
+    epochs=200,
     patience=10,
     resume=True,
     single_cls=True,
-    device="mps",
     max_det=1,
-    val=False,
-    batch=-1,
+    batch=80,
 )  # train the model
 metrics = model.val(
-    max_det=1, device="mps"
+    max_det=1
 )  # evaluate model performance on the validation set
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 # path = model.export(format="onnx")  # export the model to ONNX format
